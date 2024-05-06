43e2e84ca89a5de43d6017a1736d98565b436621
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2024-05-07 01:38:11 +0300

    Add scroll animations to PatientListScreen

---
+++ app/src/main/java/com/codlin/cardiai/presentation/components/IsScrollingUp.kt
@@ -0,0 +1,31 @@
+package com.codlin.cardiai.presentation.components
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember {
+        mutableStateOf(firstVisibleItemIndex)
+    }
+    var previousScrollOffset by remember {
+        mutableStateOf(firstVisibleItemScrollOffset)
+    }
+    return remember {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
+}
